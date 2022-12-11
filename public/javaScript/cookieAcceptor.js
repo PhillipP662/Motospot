9569afea292a0e3e5da8af29f2f9d7b8785a8f6b
PhillipP662 <phillip.p.p13@gmail.com>
2022-12-11 22:02:15 +0100

    - secure cookies

--- public/javaScript/cookieAcceptor.js
+++ public/javaScript/cookieAcceptor.js
@@ -3,7 +3,7 @@ const cookieBox = document.querySelector(".cookiewrapper"),
 const cookiebox2= document.querySelector(".cookiewrapper");
 acceptBtn.onclick = () => {
     // setting cookie for 1 day, after 1 day cookie will be expired automatically
-    document.cookie = "CookieBy=Motospot; max-age="+60*60*24;
+    document.cookie = "CookiesAccepted=true; max-age="+60*60*24+"; secure; samesite=lax; Secure";
     if (document.cookie) { // if the above cookie set
         cookieBox.classList.add("hide"); // hide cookie box once cookie set
     } else {
@@ -12,7 +12,7 @@ acceptBtn.onclick = () => {
 }
 
 // let's hide the cookie box if cookie is set and not expired
-let checkCookie = document.cookie.indexOf("CookieBy=Motospot"); // checking our set cookie
+let checkCookie = document.cookie.indexOf("CookiesAccepted=true"); // checking our set cookie
 // if cookie is set then hide the cookie box else show it
 if (checkCookie !== -1) {
     cookieBox.classList.add("hide");
